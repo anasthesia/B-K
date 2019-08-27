7dd72d77330c9e1bbad5dd83ae11e1159a7654fe
anasthesia <filimonovastasya@gmail.com>
2019-08-27 11:05:10 +0200

    nonimportant changes

--- Experimental_files/parameter_scan.py
+++ Experimental_files/parameter_scan.py
@@ -5,16 +5,31 @@ from numpy import genfromtxt
 import subprocess
 import sys
 
+#run python file-with-masses&taus. Output stored in the output folder
+
+#reads m and tau from input file, runs root macro to get BR and writes (m, tau, BRexp) to a file 
 inputfile=sys.argv[1]
-params = genfromtxt(inputfile,  comments="#", delimiter=' ')
+params = genfromtxt(inputfile,  comments="#", delimiter='\t')
 print(params)
 
-outputfile='output/'+re.sub(".dat","_exp.dat",inputfile)
+outputfile=re.sub(".dat","_exp.dat",re.sub("\A([\S]+)/",'output/',inputfile))
 print("output: "+outputfile)
 
 with open(outputfile,'w') as fout:
-fout.write("m [GeV]  tau [ps] BR(B+->K+mumu)")
+    fout.write("#m [GeV]  tau [ps] BRexp(B+->K+mumu)")
 
-for i in range(masses.shape[0]):
-	cmd='root -b'+' && .x BRmtau.C({mass},{tau},{outfile})'.format(mass=,tau=,)+' && .q'
+for i in range(params.shape[0]):
+	cmd="root -b -q  BRmtau.C'({mass},{tau},\"{outfile}\")'".\
+            format(mass=params[i,0]*10**3,tau=params[i,2],outfile=outputfile)
 	subprocess.call([cmd],shell=True)
+
+# calculates if BRtheory is allowed and combines together (m, alpha, tau, BRtheory, BRexp, allowed/not allowed)
+
+expparams=genfromtxt(outputfile,  comments="#", delimiter='\t') 
+
+for i in range(params.shape[0]):
+    ifallowed=0
+    if (params[i,3]<expparams[i,2]):
+        ifallowed=1
+
+combinedoutput=re.sub(".dat","_comb.dat",re.sub("\A([\S]+)/",'output_combined/',inputfile))
