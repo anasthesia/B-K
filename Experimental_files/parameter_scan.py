135314558ac555d94458c0112e837bca4dfa98d0
ls194@uni-heidelberg.de <hd_ls194@uc1n996.localdomain>
2019-08-27 10:45:31 +0200

    Generation of combined table with allowed/excluded label

--- Experimental_files/parameter_scan.py
+++ Experimental_files/parameter_scan.py
@@ -7,6 +7,7 @@ import sys
 
 #run python file-with-masses&taus. Output stored in the output folder
 
+#reads m and tau from input file, runs root macro to get BR and writes (m, tau, BRexp) to a file 
 inputfile=sys.argv[1]
 params = genfromtxt(inputfile,  comments="#", delimiter='\t')
 print(params)
@@ -15,9 +16,29 @@ outputfile=re.sub(".dat","_exp.dat",re.sub("\A([\S]+)/",'output/',inputfile))
 print("output: "+outputfile)
 
 with open(outputfile,'w') as fout:
-    fout.write("#m [GeV]  tau [ps] BRexp(B+->K+mumu)")
+    fout.write("#m [GeV]  tau [ps] BRexp(B+->K+mumu)\n")
 
 for i in range(params.shape[0]):
 	cmd="root -b -q  BRmtau.C'({mass},{tau},\"{outfile}\")'".\
             format(mass=params[i,0]*10**3,tau=params[i,2],outfile=outputfile)
 	subprocess.call([cmd],shell=True)
+
+# calculates if BRtheory is allowed and combines together (m, alpha, tau, BRtheory, BRexp, allowed/not allowed)
+
+expparams=genfromtxt(outputfile,  comments="#", delimiter='\t') 
+
+combooutput=re.sub(".dat","_comb.dat",re.sub("\A([\S]+)/",'output_combined/',inputfile))
+with open(combooutput,'w') as foutcomb:
+    allparams=[]
+    foutcomb.write("#m [GeV]  alpha  tau [ps] BRtheo(B+->K+mumu) BRexp allowed/excluded(1/0)\n")
+    
+    for i in range(params.shape[0]):
+        ifallowed=0
+        if (float(params[i,3])<float(expparams[i,2])):
+            ifallowed=1
+        newparams=np.append(params[i],[expparams[i,2],ifallowed])
+        allparams.append(newparams)
+    allparams=np.array(allparams)
+    #print(allparams)
+    np.savetxt(foutcomb, allparams, fmt='%5e', delimiter='\t')
+
